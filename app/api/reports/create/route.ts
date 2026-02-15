3f58595f7923c211ce3ac86363e7830bcb134c0a
raihannurhidayat <grandrx041@gmail.com>
2026-02-15 13:10:31 +0700

    fix post api from create report

--- app/api/reports/create/route.ts
+++ app/api/reports/create/route.ts
@@ -4,7 +4,7 @@ import { prisma } from "@/lib/prisma";
 import { ReportType } from "@prisma/client";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function POST(req: NextRequest, res: NextResponse) {
+export async function POST(req: NextRequest) {
   try {
     const { reportId, type, specificType, title, description, image, status } =
       await req.json();
