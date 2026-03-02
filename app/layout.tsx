8a34c3511d5c8b9debe016a827bad1fdf6dce1cf
raihannurhidayat <grandrx041@gmail.com>
2026-03-02 23:18:10 +0700

    feature: change theme color

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,17 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import { Providers } from "./Provider";
+import { Geist_Mono, JetBrains_Mono } from "next/font/google";
 
-const inter = Inter({
+const fontSans = Geist_Mono({
   subsets: ["latin"],
-  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-sans",
+});
+
+const fontMono = JetBrains_Mono({
+  subsets: ["latin"],
+  variable: "--font-mono",
 });
 
 export const metadata: Metadata = {
@@ -21,7 +26,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${inter.className} antialiased`}>
+      <body className={`${fontSans.variable} ${fontMono.variable} antialiased`}>
         <div className="relative min-h-screen bg-black selection:bg-sky-500">
           <div className="fixed inset-0 -z-10 min-h-screen">
             <div className="absolute inset-0 h-full bg-[radial-gradient(circle_at_center, rgba(56,189,248,0.03), transparent_50%)]" />
